854ae358deef3d8de2b5605d3a67802ee7d63d87
Yaa Kumiwaa Gyekye <yaagyekye17@gmail.com>
2021-10-30 23:17:04 +0000

    finish login screen

--- App.jsx
+++ App.jsx
@@ -1,23 +1,51 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-// import AppLoading from 'expo-app-loading';
+import AppLoading from "expo-app-loading";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Screens } from "./navigations/screens";
 import { Home } from "./screens/Home";
 import { Login } from "./screens/Login";
 import { ProductDetail } from "./screens/ProductDetail";
 import { Cart } from "./screens/Cart";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  Poppins_700Bold,
+  Poppins_800ExtraBold,
+} from "@expo-google-fonts/poppins";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName={Screens.Login}>
-        <Stack.Screen name={Screens.Home} component={Home} />
-        <Stack.Screen name={Screens.Login} component={Login} options={{headerShown:false}} />
-        <Stack.Screen name={Screens.ProductDetail} component={ProductDetail} />
-        <Stack.Screen name={Screens.Cart} component={Cart} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  let [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+    Poppins_800ExtraBold,
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else
+    return (
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName={Screens.Login}>
+            <Stack.Screen name={Screens.Home} component={Home} />
+            <Stack.Screen
+              name={Screens.Login}
+              component={Login}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name={Screens.ProductDetail}
+              component={ProductDetail}
+            />
+            <Stack.Screen name={Screens.Cart} component={Cart} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    );
 }
--- screens/Login.jsx
+++ screens/Login.jsx
@@ -1,12 +1,76 @@
-import React from "react";
-import { View, StyleSheet, SafeAreaView, Text } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
+import { Screens } from "../navigations/screens";
 
-export const Login = ({}) => {
+export const Login = ({ navigation }) => {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  function login() {
+    if (!username || !password) {
+      return;
+    }
+    navigation.navigate(Screens.Home);
+  }
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>SIGN IN</Text>
-    </SafeAreaView>
+    <View style={styles.container} edges={["top"]}>
+      <ImageBackground
+        source={require("../assets/Vector.png")}
+        style={{
+          width: "100%",
+          height: "37%",
+          paddingLeft: 20,
+          paddingTop: 20,
+        }}
+        imageStyle={{ width: "100%", height: "100%" }}
+        resizeMode={"cover"}
+      >
+        <Text style={styles.text}>Sign In,</Text>
+        <Text style={{ fontFamily: "Poppins_500Medium", color: "white" }}>
+          Enter your credentials to login
+        </Text>
+      </ImageBackground>
+      <View style={{ marginTop: 50, paddingHorizontal: 20 }}>
+        <Label title="Username" />
+        <View style={styles.inputContainer}>
+          <TextInput
+            value={username}
+            onChangeText={(text) => setUsername(text)}
+          />
+        </View>
+        <Label title="Password" />
+        <View style={styles.inputContainer}>
+          <TextInput
+            secureTextEntry
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+          />
+        </View>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={login}>
+            <Text
+              style={{
+                fontFamily: "Poppins_600SemiBold",
+                color: "white",
+                fontSize: 18,
+              }}
+            >
+              Sign In
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
   );
+};
+const Label = function ({ title }) {
+  return <Text style={styles.label}>{title}</Text>;
 };
 
 const styles = StyleSheet.create({
@@ -16,8 +80,39 @@ const styles = StyleSheet.create({
   },
 
   text: {
+    marginTop: 80,
     textAlign: "left",
-    fontWeight: "bold",
-    fontSize: 46,
+    fontFamily: "Poppins_600SemiBold",
+    fontSize: 35,
+    color: "white",
+  },
+  label: {
+    color: "#75BA8B",
+    marginBottom: 20,
+    marginVertical: 20,
+    fontSize: 14,
+    fontFamily: "Poppins_500Medium",
+  },
+  input: {
+    borderBottomColor: "#000000",
+    borderBottomWidth: 2,
+    borderWidth: 1,
+  },
+  inputContainer: {
+    borderBottomColor: "#EBECEF",
+    borderBottomWidth: 2,
+    justifyContent: "flex-end",
+  },
+  buttonContainer: {
+    alignItems: "center",
+    marginTop: 50,
+  },
+  button: {
+    backgroundColor: "#50A462",
+    width: "60%",
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 50,
   },
 });
